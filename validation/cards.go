4fcc826255614691aa96c3f982e071cbc6df4794
= <adverted.media@gmail.com>
2020-03-13 05:09:57 +0800

    Add pattern and length match on an INVDN return response

--- validation/cards.go
+++ validation/cards.go
@@ -249,7 +249,7 @@ func Validate(creditCardNumber string) *TopResult {
 
 	if !luhn.IsValid(creditCardNumber) {
 		e = Error{INVDN, fmt.Sprintf(InvalidNumber, results[0].name)}
-		return &TopResult{Name: results[0].name, Error: e}
+		return &TopResult{Name: results[0].name, PatternMatch: results[0].patternMatch, LengthMatch: results[0].lengthMatch, Error: e}
 	}
 
 	e = Error{SUCCS, "Success"}
